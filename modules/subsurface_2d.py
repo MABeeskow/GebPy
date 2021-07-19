1f57526cfa423c8b0a9131a6a0e704f4b42c1464
Maximilian Beeskow <maximilian.beeskow@rwth-aachen.de>
2021-07-19 16:33:16 +0200

    updated subsurface section

--- modules/subsurface_2d.py
+++ modules/subsurface_2d.py
@@ -15,6 +15,7 @@ import numpy as np
 import scipy.interpolate as interp
 from numpy import round
 import random as rd
+from itertools import chain
 from random import randint
 from modules.carbonates import limestone, dolomite
 from modules.siliciclastics import sandstone, shale, ore, Soil
@@ -90,20 +91,89 @@ class Surface:
 
 class Units:
     #
-    def __init__(self, x_values):
-        self.x_values = x_values
+    def __init__(self, x_limits, y_limits, number):
+        self.x_limits = x_limits
+        self.y_limits = y_limits
+        self.number = number
     #
-    def create_units(self, f_surface):
-        n_units = len(self.x_values)
-        self.y_values = f_surface(self.x_values)
-        print("x:", self.x_values)
-        print("y:", self.y_values)
-        f_surface1 = f_surface.derivative(nu=1)
-        derivatives = f_surface1(self.x_values)
-        print("Derivatives", derivatives)
-        a_pos = max(derivatives)
-        a_neg = min(derivatives)
-        if abs(a_pos) >= abs(a_neg):
-            print("Steepest point:", a_pos)
-        else:
-            print("Steepest point:", a_neg)
+    def create_outcrops(self, extrema):
+        condition = False
+        while condition == False:
+            x_outcrops = np.sort(rd.sample(list(range(int(self.x_limits[0]*(1-0.05)),
+                                                      int(extrema[-1][0]-5))) + list(range(int(extrema[-1][0]+5),
+                                                      int(self.x_limits[1]*(1-0.05)))), self.number))
+            x_diff = np.diff(x_outcrops)
+            check = any(item in np.arange(0, 5) for item in x_diff)
+            if check == False:
+                condition = True
+            else:
+                continue
+        #
+        return x_outcrops
+    #
+    def create_units(self, f_surface, kind=None):
+        x_units = []
+        y_units = []
+        f1 = interp.CubicSpline.derivative(f_surface)
+        f2 = interp.CubicSpline.derivative(f1)
+        x_extreme = np.around(f1.solve(), 4)
+        y_extreme = np.around(f_surface(x_extreme), 4)
+        extrema_type = np.around(f2(x_extreme), 4)
+        data_extrema = []
+        for i in range(len(x_extreme)):
+            if extrema_type[i] < 0:
+                data_extrema.append([round(x_extreme[i], 2), round(y_extreme[i], 2), "Maximum"])
+            elif extrema_type[i] > 0:
+                data_extrema.append([round(x_extreme[i], 2), round(y_extreme[i], 2), "Minimum"])
+            elif extrema_type[i] == 0:
+                data_extrema.append([round(x_extreme[i], 2), round(y_extreme[i], 2), "Saddle"])
+        if len(x_extreme) == 0:
+            data_extrema.append([round(max(self.x_limits), 2), round(max(self.y_limits), 2), "Maximum"])
+            data_extrema.append([round(min(self.x_limits), 2), round(min(self.y_limits), 2), "Minimum"])
+        data_extrema = np.array(data_extrema, dtype="object")
+        data_extrema = data_extrema[data_extrema[:, 1].argsort()]
+        print("Global Minimum:", data_extrema[0])
+        print("Global Maximum:", data_extrema[-1])
+
+        self.x_outcrops = self.create_outcrops(extrema=data_extrema)
+        self.y_outcrops = f_surface(self.x_outcrops)
+        n_units = len(self.x_outcrops)
+
+        if kind == None:
+            x_delta = rd.randint(-25, 25)
+            self.x_outcrops_bottom = self.x_outcrops + x_delta
+            y_min = min(self.y_limits)
+            for i in range(n_units):
+                if self.x_outcrops is not self.x_outcrops_bottom:
+                    x1 = self.x_outcrops[i]
+                    x2 = self.x_outcrops_bottom[i]
+                    y1 = self.y_outcrops[i]
+                    y2 = y_min
+                    b = (x1*y2 - x2*y1)/(x1 - x2)
+                    if x1 != 0:
+                        a = (y1 - b)/x1
+                    else:
+                        a = (y2 - b)/x2
+                    x = np.linspace(self.x_outcrops[i], self.x_outcrops_bottom[i], 25+1)
+                    upper_limit = np.argwhere(x > self.x_limits[-1])
+                    if len(upper_limit) > 0:
+                        x = x[:upper_limit[0][0]]
+                    x_units.append(x)
+                    y_units.append(a*x + b)
+                else:
+                    x1 = self.x_outcrops[i]
+                    x2 = self.x_outcrops_bottom[i]
+                    y1 = self.y_outcrops[i]
+                    y2 = y_min
+                    x_units.append(np.array([x1, x2]))
+                    y_units.append(np.array([y1, y2]))
+        elif kind == "horizontal":
+            for i in range(len(self.x_outcrops)):
+                if self.x_outcrops[i] < data_extrema[-1][0]:
+                    x_units.append([self.x_outcrops[i], f_surface.solve(y=self.y_outcrops[i])[2]])
+                    y_units.append([self.y_outcrops[i], self.y_outcrops[i]])
+                elif self.x_outcrops[i] > data_extrema[-1][0]:
+                    x_units.append([self.x_outcrops[i], f_surface.solve(y=self.y_outcrops[i])[1]])
+                    y_units.append([self.y_outcrops[i], self.y_outcrops[i]])
+        #
+        return x_units, y_units, self.x_outcrops
--- tests/test_subsurface_2d.py
+++ tests/test_subsurface_2d.py
@@ -19,17 +19,16 @@ from modules import subsurface_2d
 ## CLASS
 class TestingSubsurface:
     #
-    def __init__(self, coordinates, degree):
+    def __init__(self, coordinates, degree, kind=None):
         self.coordinates = coordinates
         self.degree = degree
 
         x_left = self.coordinates[0][0]
         x_right = self.coordinates[-1][0]
-        x = np.linspace(x_left, x_right, 25+1)
-
+        x = np.linspace(x_left, x_right, 50+1)
         condition = False
         while condition == False:
-            x_outcrops = np.sort(rd.sample(range(int(x_left*(1+0.05)), int(x_right*(1-0.05))), 5))
+            x_outcrops = np.sort(rd.sample(range(int(x_left*(1-0.05)), int(x_right*(1-0.05))), 10))
             x_diff = np.diff(x_outcrops)
             check = any(item in np.arange(0, 5) for item in x_diff)
             if check == False:
@@ -43,35 +42,45 @@ class TestingSubsurface:
             a, b = subsurface_2d.Surface(coordinates=self.coordinates).create_linear_surface()
             y = a*x + b
             y_outcrops = a*x_outcrops + b
-            ax.scatter(x, y, label="Surface", zorder=2)
-            ax.plot(x, y, zorder=1)
-            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=2)
+
+            ax.plot(x, y, label="Surface", color="C0", zorder=1)
+            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=2, color="C1")
         elif self.degree == "quadratic":
             a, b, c = subsurface_2d.Surface(coordinates=self.coordinates).create_quadratic_surface()
             y = a*x**2 + b*x + c
             y_outcrops = a*x_outcrops**2 + b*x_outcrops + c
-            ax.scatter(x, y, label="Surface", zorder=2)
-            ax.plot(x, y, zorder=1)
-            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=2)
+
+            ax.plot(x, y, label="Surface", color="C0", zorder=1)
+            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=2, color="C1")
         elif self.degree == "interpolated":
             f = subsurface_2d.Surface(coordinates=self.coordinates).create_interpolated_surface()
-            subsurface_2d.Units(x_values=x_outcrops).create_units(f_surface=f)
             y = f(x)
+            if kind == None:
+                x_units, y_units, x_outcrops = subsurface_2d.Units(x_limits=[x[0], x[-1]], y_limits=[y[0], y[-1]],
+                                                               number=10).create_units(f_surface=f)
+            else:
+                x_units, y_units, x_outcrops = subsurface_2d.Units(x_limits=[x[0], x[-1]], y_limits=[y[0], y[-1]],
+                                                               number=10).create_units(f_surface=f, kind=kind)
             y_outcrops = f(x_outcrops)
 
-            ax.plot(x, y, label="Surface", zorder=1)
-            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=2, color="orangered")
+            ax.plot(x, y, label="Surface", color="black", zorder=2)
+            ax.scatter(x_outcrops, y_outcrops, label="Outcrops", zorder=3, color="C1")
+
+            for i in range(len(y_units)):
+                ax.plot(x_units[i], y_units[i], linestyle="dashed", zorder=1,
+                        label=format("Top Unit "+str(i+1)))
 
         ax.grid()
         ax.set_xlabel("x (m)")
         ax.set_ylabel("y (m)")
-        ax.set_xticks(np.arange(x_left, x_right+10, 10))
-        ax.legend(loc="upper left")
+        ax.set_xticks(np.arange(x_left, x_right+10, 20))
+        ax.legend(loc="upper left", prop={'size': 8})
         ax.set_axisbelow(True)
         plt.show()
 
 # PROGRAM
-TestingSubsurface(coordinates=[[-50, 0], [50, 10]], degree="linear")
-TestingSubsurface(coordinates=[[-50, 0], [0, 20], [50, 0]], degree="quadratic")
-TestingSubsurface(coordinates=[[-50, 0], [-10, 20], [20, 25], [50, 5]], degree="interpolated")
-TestingSubsurface(coordinates=[[-50, 0], [50, 10]], degree="interpolated")
+TestingSubsurface(coordinates=[[-100, 0], [100, 10]], degree="linear")
+TestingSubsurface(coordinates=[[-100, 0], [0, 20], [100, 0]], degree="quadratic")
+TestingSubsurface(coordinates=[[-100, 0], [-10, 20], [20, 25], [100, 5]], degree="interpolated")
+TestingSubsurface(coordinates=[[-100, 0], [100, 10]], degree="interpolated")
+TestingSubsurface(coordinates=[[-100, 0], [-10, 20], [20, 25], [100, 5]], degree="interpolated", kind="horizontal")
