5ef709308ad58b93646d5295970df2a747aef146
mabeeskow <MABeeskow@googlemail.com>
2022-02-08 16:09:56 +0100

    "updated setup.py"

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from setuptools import setup
-setup(name="GebPy", packages=["modules"])
 
-from GebPy import modules
+setup(name="modules", packages=["modules"], data_files=["../documents/readme_images/GebPy_Logo.png"])
+
+import modules
+from modules.gui_elements import SimpleElements as SE
+from modules.sulfates import Sulfates
+from modules.oxides import Oxides
+from modules.sulfides import Sulfides
+from modules.carbonates import Carbonates
+from modules.halogenes import Halogenes
+from modules.silicates import Tectosilicates, Phyllosilicates, Nesosilicates, Sorosilicates, Inosilicates
+from modules.phosphates import Phosphates
+from modules.siliciclastics import sandstone, shale
+from modules.carbonates import limestone, dolomite
+from modules.ore import Ores
+from modules.igneous import Plutonic
+from modules.evaporites import Evaporites
+from modules.sequences import DataProcessing as DP
