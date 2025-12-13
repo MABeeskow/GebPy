d4e72979c8cecf31be834e16f36af113c3748421
Maximilian Beeskow <maximilian.beeskow@rwth-aachen.de>
2025-12-13 17:56:07 +0100

    "updated the 'rock generation' class"

--- src/gebpy/core/rocks/common.py
+++ src/gebpy/core/rocks/common.py
@@ -6,7 +6,7 @@
 # Name:		common.py
 # Author:	Maximilian A. Beeskow
 # Version:	1.0
-# Date:		03.12.2025
+# Date:		13.12.2025
 
 #-----------------------------------------------
 
@@ -14,3 +14,74 @@
 Module: common.py
 This module contains several routines that are commonly used by the different rock-related modules.
 """
+
+# PACKAGES
+import re
+
+# MODULES
+from modules.chemistry import PeriodicSystem
+
+class RockGeneration:
+    def __init__(self):
+        self.elements = {
+            "H": PeriodicSystem(name="H").get_data(),
+            "C": PeriodicSystem(name="C").get_data(),
+            "O": PeriodicSystem(name="O").get_data(),
+            "Na": PeriodicSystem(name="Na").get_data(),
+            "Mg": PeriodicSystem(name="Mg").get_data(),
+            "Al": PeriodicSystem(name="Al").get_data(),
+            "Si": PeriodicSystem(name="Si").get_data(),
+            "S": PeriodicSystem(name="S").get_data(),
+            "Cl": PeriodicSystem(name="Cl").get_data(),
+            "K": PeriodicSystem(name="K").get_data(),
+            "Ca": PeriodicSystem(name="Ca").get_data(),
+            "Mn": PeriodicSystem(name="Mn").get_data(),
+            "Fe": PeriodicSystem(name="Fe").get_data(),
+            "Ni": PeriodicSystem(name="Ni").get_data()}
+
+    def _parse_formula(self, formula: str):
+        pattern = r"([A-Z][a-z]?)(\d*)"
+        matches = re.findall(pattern, formula)
+
+        composition = {}
+        for elem, amount in matches:
+            amount = int(amount) if amount else 1
+            composition[elem] = composition.get(elem, 0) + amount
+
+        return composition
+
+    def _get_cation_element(self, oxide: str) -> str:
+        first = oxide[0]
+        if len(oxide) > 1 and oxide[1].islower():
+            return oxide[:2]
+
+        return first
+
+    def _get_anion_element(self, compound: str) -> str:
+        elements = re.findall(r"[A-Z][a-z]?", compound)
+        last = elements[-1]
+        return last
+
+    def _determine_oxide_conversion_factors(self):
+        list_oxides = [
+            "H2O", "CO", "CO2", "Na2O", "MgO", "Al2O3", "SiO2", "Cl2O", "K2O", "CaO", "MnO", "Mn2O3", "MnO2", "MnO3",
+            "Mn2O7", "FeO", "Fe2O3", "FeO3", "NiO", "Ni2O3", "TiO2", "Ti2O3", "VO", "V2O3", "VO2", "V2O10", "CrO",
+            "Cr2O3", "CrO3", "CoO", "Co2O3", "Cu2O", "CuO", "ZnO", "GeO2", "As2O3", "As2O10", "ZrO2", "Nb2O3", "Nb2O10",
+            "MoO", "Mo2O3", "MoO2", "Mo2O10", "MoO3", "CdO", "SnO", "SnO2", "Sb2O3", "Sb2O10", "TeO2", "TeO3", "Ta2O10",
+            "WO", "W2O3", "WO2", "W2O10", "WO3", "Au2O", "Au2O3", "Tl2O", "Tl2O3", "PbO", "PbO2", "Bi2O3", "Bi2O10",
+            "U2O3", "UO2", "U2O10", "UO3", "Nb2O5", "Ta2O5", "SO", "SO2", "SO3"]
+        mass_oxygen = self.elements["O"][2]
+        _conversion_factors = {}
+        for oxide in list_oxides:
+            _conversion_factors[oxide] = self._parse_formula(formula=oxide)
+            cation = self._get_cation_element(oxide=oxide)
+            if cation in self.elements:
+                mass_cation = self.elements[cation][2]
+                _conversion_factors[oxide]["factor"] = (_conversion_factors[oxide][cation]*mass_cation +
+                                                        _conversion_factors[oxide]["O"]*mass_oxygen)/(
+                        _conversion_factors[oxide][cation]*mass_cation)
+            else:
+                pass
+                #print(self.name, ": cation", cation, "not found in chemical container.")
+
+        return _conversion_factors
--- src/gebpy/core/rocks/sedimentary.py
+++ src/gebpy/core/rocks/sedimentary.py
@@ -6,7 +6,7 @@
 # Name:		sedimentary.py
 # Author:	Maximilian A. Beeskow
 # Version:	1.0
-# Date:		04.12.2025
+# Date:		13.12.2025
 
 #-----------------------------------------------
 
@@ -23,6 +23,7 @@ from pathlib import Path
 
 # MODULES
 from src.gebpy.core.minerals.synthesis import MineralDataGeneration
+from src.gebpy.core.rocks.common import RockGeneration
 
 # Code
 BASE_PATH = Path(__file__).resolve().parents[2]
@@ -40,6 +41,7 @@ class SedimentaryRocks:
         self.current_seed = int(np.round(self.rng.uniform(0, 1000), 0))
         self.rounding = rounding
         self.data_path = DATA_PATH
+        self.conversion_factors = RockGeneration()._determine_oxide_conversion_factors()
 
     def _load_yaml(self, rock_name: str) -> dict:
         # 1) Cache-Hit
@@ -121,19 +123,118 @@ class SedimentaryRocks:
 
         return x
 
-    def _weighted_sum(self, dfs, fractions):
-        bulk = None
+    def _collect_mineral_data(self, list_minerals, number):
+        _mineral_data = []
+        for index, mineral in enumerate(list_minerals):
+            data_init = MineralDataGeneration(mineral, number)
+            data_mineral = data_init.generate_data()
+            is_fixed = data_mineral.shape[0] == 1
+            if is_fixed and number > 1:
+                data_mineral = pd.concat([data_mineral]*number, ignore_index=True)
+            elif not is_fixed and data_mineral.shape[0] != number:
+                raise ValueError(
+                    f"Mineral '{mineral}' returned {data_mineral.shape[0]} rows, "
+                    f"but expected {number}.")
+            _mineral_data.append(data_mineral)
 
-        for df, frac in zip(dfs, fractions):
-            numeric_cols = df.select_dtypes(include=np.number).columns
-            weighted = df[numeric_cols]*frac
+        return _mineral_data
 
-            if bulk is None:
-                bulk = weighted
+    def _extract_mineral_property_data(self, list_minerals, data_mineral, property):
+        _helper = []
+        for index, mineral in enumerate(list_minerals):
+            dataset = data_mineral[index][property]
+            _helper.append(list(dataset))
+
+        return np.array(_helper)
+
+    def _update_bulk_density_data(self, _bulk_data, density_fluid, number):
+        porosity = _bulk_data["porosity"]
+        _bulk_data["rho_s"] = _bulk_data["rho"]
+        _bulk_data["rho"] = (1 - porosity)*_bulk_data["rho"] + porosity*density_fluid
+        _bulk_data["rho_f"] = np.ones(number)*density_fluid
+
+        return _bulk_data
+
+    def _update_seismic_velocities(self, _bulk_data):
+        val_G = _bulk_data["G"]*10**9
+        val_rho = _bulk_data["rho"]
+        val_K = _bulk_data["K"]*10**9
+        _bulk_data["vS"] = (val_G/val_rho)**0.5
+        _bulk_data["vP"] = ((val_K + 4/3*val_G)/val_rho)**0.5
+        _bulk_data["vP/vS"] = _bulk_data["vP"]/_bulk_data["vS"]
+
+        return _bulk_data
+
+    def _update_elastic_parameter_data(self, _bulk_data):
+        val_G = _bulk_data["G"]*10**9
+        val_K = _bulk_data["K"]*10**9
+        _bulk_data["E"] = ((9*val_K*val_G)/(3*val_K + val_G))*1e-9
+        _bulk_data["poisson"] = (3*val_K - 2*val_G)/(2*(3*val_K + val_G))
+        _bulk_data["lame"] = (val_K - (2*val_G)/3)*1e-9
+
+        return _bulk_data
+
+    def _extract_element_data(self, data_minerals, list_elements):
+        for index, dataset in enumerate(data_minerals):
+            list_keys_i = list(dataset.keys())
+            filtered = [x[len("chemistry."):] for x in list_keys_i if x.startswith("chemistry.")]
+            for element in filtered:
+                if element not in list_elements:
+                    list_elements.append(element)
+
+        return list_elements
+
+    def _extract_oxide_data(self, data_minerals, list_oxides):
+        for index, dataset in enumerate(data_minerals):
+            list_keys_i = list(dataset.keys())
+            filtered = [x[len("compounds."):] for x in list_keys_i if x.startswith("compounds.")]
+            for element in filtered:
+                if element not in list_oxides:
+                    list_oxides.append(element)
+            if dataset["mineral"][0] in ["Py"]:
+                list_oxides.append("Fe2O3")
+                list_oxides.append("SO3")
+                list_oxides.remove("FeS2")
+
+        return list_oxides
+
+    def _update_chemistry_data(self, _bulk_data, data_minerals, data_composition, element, number):
+        _helper = [[] for _ in range(len(data_minerals))]
+        key_element = "chemistry." + element
+        for index, dataset in enumerate(data_minerals):
+            if key_element in list(dataset.keys()):
+                data = list(dataset[key_element])
+                _helper[index] = data
             else:
-                bulk = bulk.add(weighted, fill_value=0)
+                data = [0]*number
+                _helper[index] = data
+        _helper = np.array(_helper)
+        _helper_bulk = np.sum(data_composition*_helper.T, axis=1)
+        key_element = "w." + element
+        _bulk_data[key_element] = np.array(_helper_bulk)
 
-        return bulk
+        return _bulk_data
+
+    def _update_oxide_data(self, bulk_data, list_oxides):
+        for oxide in list_oxides:
+            cation = RockGeneration()._get_cation_element(oxide=oxide)
+            anion = RockGeneration()._get_anion_element(compound=oxide)
+            if anion == "O":
+                key_cation = "w." + cation
+                values = self.conversion_factors[oxide]["factor"]*bulk_data[key_cation]
+                key_oxide = "w." + oxide
+                bulk_data[key_oxide] = values
+            else:
+                print("There is a non-oxide compound part of the list.")
+
+        return bulk_data
+
+    def _assign_mineral_amounts(self, bulk_data, data_amounts):
+        for mineral, values in data_amounts.items():
+            key_mineral = "phi." + mineral
+            bulk_data[key_mineral] = values
+
+        return bulk_data
 
     def generate_dataset(self, number: int = 1, fluid: str = "water", density_fluid=None) -> None:
         if density_fluid is None:
@@ -157,49 +258,55 @@ class SedimentaryRocks:
             _limits["upper"].append(values[1])
 
         list_minerals = list(SedimentaryRocks._mineralogy_cache[self.name].keys())
-        mineral_amounts = self._sample_bounded_simplex(min_vals=_limits["lower"], max_vals=_limits["upper"])
         _properties = ["rho", "vP", "vS", "K", "G", "GR", "PE"]
         _bulk_data = {}
-        _mineral_data = []
-
-        for index, mineral in enumerate(list_minerals):
-            data_init = MineralDataGeneration(mineral, number)
-            data_mineral = data_init.generate_data()
-            is_fixed = data_mineral.shape[0] == 1
-
-            if is_fixed and number > 1:
-                data_mineral = pd.concat([data_mineral]*number, ignore_index=True)
-            elif not is_fixed and data_mineral.shape[0] != number:
-                raise ValueError(
-                    f"Mineral '{mineral}' returned {data_mineral.shape[0]} rows, "
-                    f"but expected {number}.")
-
-            _mineral_data.append(data_mineral)
-
-        _bulk_data = self._weighted_sum(dfs=_mineral_data, fractions=mineral_amounts)
-        for index, (mineral, frac) in enumerate(zip(list_minerals, mineral_amounts)):
-            key = _mineral_data[index]["mineral"][0]
-            _bulk_data[f"phi.{key}"] = frac
-        # Density adjustment due to porosity
-        _bulk_data["porosity"] = porosity
-        _bulk_data["rho_s"] = _bulk_data["rho"]
-        _bulk_data["rho"] = (1 - porosity)*_bulk_data["rho"] + porosity*density_fluid
-        _bulk_data["rho_f"] = np.ones(number)*density_fluid
-        # Value conversion
-        val_K = _bulk_data["K"]*1e9
-        val_G = _bulk_data["G"]*1e9
-        val_rho = _bulk_data["rho"]
-        # Seismic velocities
-        _bulk_data["vS"] = (val_G/val_rho)**0.5
-        _bulk_data["vP"] = ((val_K + 4/3*val_G)/val_rho)**0.5
-        _bulk_data["vP/vS"] = _bulk_data["vP"]/_bulk_data["vS"]
-        # Elastic moduli
-        _bulk_data["E"] = ((9*val_K*val_G)/(3*val_K + val_G))*1e-9
-        _bulk_data["poisson"] = (3*val_K - 2*val_G)/(2*(3*val_K + val_G))
-        _bulk_data["lame"] = (val_K - (2*val_G)/3)*1e-9
-        # Cleanup
-        _bulk_data.drop(columns=["nu"], errors="ignore", inplace=True)
-        _bulk_data = _bulk_data.rename(columns=lambda c: c.replace("chemistry.", "w.") )
+        # Collect mineralogical composition data
+        _helper_amounts = {}
+        _helper_composition = []
+        _helper_mineral_amounts = {}
+        for index_main in range(number):
+            mineral_amounts = self._sample_bounded_simplex(min_vals=_limits["lower"], max_vals=_limits["upper"])
+            _helper_composition.append(list(mineral_amounts))
+            _helper_dict = {}
+            for j, mineral in enumerate(list_minerals):
+                _helper_dict[mineral] = mineral_amounts[j]
+                if mineral not in _helper_mineral_amounts:
+                    _helper_mineral_amounts[mineral] = []
+                _helper_mineral_amounts[mineral].append(mineral_amounts[j])
+            _helper_amounts[index_main] = _helper_dict
+        _helper_composition = np.array(_helper_composition)
+        # Collect mineral data
+        _helper_elements = []
+        _helper_oxides = []
+        _mineral_data = self._collect_mineral_data(list_minerals=list_minerals, number=number)
+        _helper_elements = self._extract_element_data(data_minerals=_mineral_data, list_elements=_helper_elements)
+        _helper_oxides = self._extract_oxide_data(data_minerals=_mineral_data, list_oxides=_helper_oxides)
+        _helper_bulk_data = {}
+        for property in ["rho", "K", "G", "GR", "PE"]:
+            _helper_property = self._extract_mineral_property_data(
+                list_minerals=list_minerals, data_mineral=_mineral_data, property=property)
+            _helper_bulk_data[property] = np.sum(_helper_composition*_helper_property.T, axis=1)
+        # Assign porosity data
+        _helper_bulk_data["porosity"] = porosity
+        # Update bulk density data
+        _helper_bulk_data = self._update_bulk_density_data(
+            _bulk_data=_helper_bulk_data, density_fluid=density_fluid, number=number)
+        # Update bulk seismic velocity data
+        _helper_bulk_data = self._update_seismic_velocities(_bulk_data=_helper_bulk_data)
+        # Update elastic parameter data
+        _helper_bulk_data = self._update_elastic_parameter_data(_bulk_data=_helper_bulk_data)
+        # Update chemistry data
+        for element in _helper_elements:
+            _helper_bulk_data = self._update_chemistry_data(
+                _bulk_data=_helper_bulk_data, data_minerals=_mineral_data, data_composition=_helper_composition,
+                element=element, number=number)
+        # Update oxide data
+        _helper_bulk_data = self._update_oxide_data(bulk_data=_helper_bulk_data, list_oxides=_helper_oxides)
+        # Update rock composition data
+        _helper_bulk_data = self._assign_mineral_amounts(
+            bulk_data=_helper_bulk_data, data_amounts=_helper_mineral_amounts)
+        # Conversion to a pandas dataframe object
+        _bulk_data = pd.DataFrame(_helper_bulk_data)
 
         return _bulk_data
 
